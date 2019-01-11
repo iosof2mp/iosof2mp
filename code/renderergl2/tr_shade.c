iosof2mp/iosof2mp

commit 2073a0c6db3c7b84c8e8958f997a878ce4636691

OpenGL2: Fixes to alpha/color generation and shader loading. This commit also adds preliminary support for lightmaps in color generation and adds basic support for missing shader keywords.

code/renderergl2/tr_shade.c
@@ -434,16 +434,22 @@ static void ProjectDlightTexture( void ) {
 
 static void ComputeShaderColors( shaderStage_t *pStage, vec4_t baseColor, vec4_t vertColor, int blend )
 {
-    qboolean isBlend = ((blend & GLS_SRCBLEND_BITS) == GLS_SRCBLEND_DST_COLOR)
+    int         forceRGBGen = pStage->rgbGen;
+    qboolean    is2DDraw    = backEnd.currentEntity == &backEnd.entity2D;
+    qboolean    isBlend     = ((blend & GLS_SRCBLEND_BITS) == GLS_SRCBLEND_DST_COLOR)
         || ((blend & GLS_SRCBLEND_BITS) == GLS_SRCBLEND_ONE_MINUS_DST_COLOR)
         || ((blend & GLS_DSTBLEND_BITS) == GLS_DSTBLEND_SRC_COLOR)
         || ((blend & GLS_DSTBLEND_BITS) == GLS_DSTBLEND_ONE_MINUS_SRC_COLOR);
 
-    qboolean is2DDraw = backEnd.currentEntity == &backEnd.entity2D;
+    float       overbright  = (isBlend || is2DDraw) ? 1.0f : (float)(1 << tr.overbrightBits);
+    fog_t       *fog;
 
-    float overbright = (isBlend || is2DDraw) ? 1.0f : (float)(1 << tr.overbrightBits);
-
-    fog_t *fog;
+    //
+    // Check for night vision.
+    //
+    if(tr.visualOverlay == VO_NIGHTVISION && !backEnd.projection2D){
+        forceRGBGen = CGEN_VISUALOVERLAY;
+    }
 
     baseColor[0] =
     baseColor[1] =
@@ -458,7 +464,7 @@ static void ComputeShaderColors( shaderStage_t *pStage, vec4_t baseColor, vec4_t
     //
     // rgbGen
     //
-    switch ( pStage->rgbGen )
+    switch ( forceRGBGen )
     {
         case CGEN_EXACT_VERTEX:
         case CGEN_EXACT_VERTEX_LIT:
@@ -532,9 +538,40 @@ static void ComputeShaderColors( shaderStage_t *pStage, vec4_t baseColor, vec4_t
             break;
         case CGEN_IDENTITY:
         case CGEN_LIGHTING_DIFFUSE:
+        case CGEN_LIGHTMAP0:
             baseColor[0] =
             baseColor[1] =
             baseColor[2] = overbright;
+            break;
+        case CGEN_LIGHTMAP1:
+            VectorScale4(styleColors[pStage->lightmapStyle], 1.0f / 255.0f, baseColor);
+            break;
+        case CGEN_LIGHTMAP2:
+            VectorScale4(styleColors[pStage->lightmapStyle], 1.0f / 255.0f, baseColor);
+            break;
+        case CGEN_LIGHTMAP3:
+            VectorScale4(styleColors[pStage->lightmapStyle], 1.0f / 255.0f, baseColor);
+            break;
+        case CGEN_UNKNOWN16:
+            /*if(tess.numVertexes){
+                for(i = 0; i < tess.numVertexes; i++){
+                    // FIXME BOE
+                }
+            }*/
+            break;
+        case CGEN_VISUALOVERLAY:
+            if(backEnd.currentEntity == &tr.worldEntity){
+                baseColor[0] =
+                baseColor[1] =
+                baseColor[2] =
+                baseColor[3] = 0x32 / 255.0f;
+            //}else if(!0){ // FIXME BOE: Inverse OpenGL1 check. 0xFF is default.
+            }else{
+                baseColor[0] =
+                baseColor[1] =
+                baseColor[2] =
+                baseColor[3] = 0xAA / 255.0f;
+            }
             break;
         case CGEN_IDENTITY_LIGHTING:
         case CGEN_BAD:
@@ -549,8 +586,10 @@ static void ComputeShaderColors( shaderStage_t *pStage, vec4_t baseColor, vec4_t
         case AGEN_SKIP:
             break;
         case AGEN_CONST:
-            baseColor[3] = pStage->constantColor[3] / 255.0f;
-            vertColor[3] = 0.0f;
+            if ( forceRGBGen != CGEN_CONST ) {
+                baseColor[3] = pStage->constantColor[3] / 255.0f;
+                vertColor[3] = 0.0f;
+            }
             break;
         case AGEN_WAVEFORM:
             baseColor[3] = RB_CalcWaveAlphaSingle( &pStage->alphaWave );
@@ -577,6 +616,13 @@ static void ComputeShaderColors( shaderStage_t *pStage, vec4_t baseColor, vec4_t
         case AGEN_ONE_MINUS_VERTEX:
             baseColor[3] = 1.0f;
             vertColor[3] = -1.0f;
+            break;
+        case AGEN_BLEND:
+            /*
+            // FIXME BOE
+            if(forceRGBGen != CGEN_VERTEX){
+                colors[i][3] = tess.vertexAlphas[i][pStage->index];
+            }*/
             break;
         case AGEN_IDENTITY:
         case AGEN_LIGHTING_SPECULAR:

code/renderergl2/tr_local.h
@@ -242,6 +242,7 @@ typedef enum {
     AGEN_WAVEFORM,
     AGEN_PORTAL,
     AGEN_CONST,
+    AGEN_BLEND
 } alphaGen_t;
 
 typedef enum {
@@ -258,7 +259,13 @@ typedef enum {
     CGEN_WAVEFORM,          // programmatically generated
     CGEN_LIGHTING_DIFFUSE,
     CGEN_FOG,               // standard fog
-    CGEN_CONST              // fixed color
+    CGEN_CONST,             // fixed color
+    CGEN_LIGHTMAP0,
+    CGEN_LIGHTMAP1,
+    CGEN_LIGHTMAP2,
+    CGEN_LIGHTMAP3,
+    CGEN_UNKNOWN16,         // FIXME BOE
+    CGEN_VISUALOVERLAY      // nightvision or thermal goggles
 } colorGen_t;
 
 typedef enum {
@@ -387,6 +394,8 @@ typedef enum
 
 typedef struct {
     qboolean        active;
+    byte            index;
+    byte            lightmapStyle;
 
     textureBundle_t bundle[NUM_TEXTURE_BUNDLES];
 
@@ -424,7 +433,8 @@ typedef enum {
 typedef enum {
     FP_NONE,        // surface is translucent and will just be adjusted properly
     FP_EQUAL,       // surface is opaque but possibly alpha tested
-    FP_LE           // surface is trnaslucent, but still needs a fog pass (fog surface)
+    FP_LE,          // surface is trnaslucent, but still needs a fog pass (fog surface)
+    FP_GLFOG
 } fogPass_t;
 
 typedef struct {
@@ -1658,6 +1668,8 @@ typedef struct {
     float                   sawToothTable[FUNCTABLE_SIZE];
     float                   inverseSawToothTable[FUNCTABLE_SIZE];
     float                   fogTable[FOG_TABLE_SIZE];
+
+    visual_t                visualOverlay;      // Visual overlay support for nightvision and thermal goggles.
 } trGlobals_t;
 
 extern backEndState_t   backEnd;
@@ -2035,6 +2047,7 @@ typedef struct shaderCommands_s
     vec2_t      texCoords[SHADER_MAX_VERTEXES][NUM_TEX_COORDS] QALIGN(16);
     uint16_t    color[SHADER_MAX_VERTEXES][4] QALIGN(16);
     int16_t     lightdir[SHADER_MAX_VERTEXES][4] QALIGN(16);
+    byte        vertexAlphas[SHADER_MAX_VERTEXES][4] QALIGN(16);
     //int           vertexDlightBits[SHADER_MAX_VERTEXES] QALIGN(16);
 
     void *attribPointers[ATTR_INDEX_COUNT];

code/renderergl2/tr_shader.c
@@ -680,9 +680,6 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
             {
                 stage->bundle[0].isLightmap = qtrue;
                 if ( shader.lightmapIndex[0] < 0 || shader.lightmapIndex[0] >= tr.numLightmaps ) {
-#ifndef FINAL_BUILD
-                    ri.Printf(PRINT_ALL, S_COLOR_RED "Lightmap requested but none avilable for shader '%s'\n", shader.name);
-#endif // FINAL_BUILD
                     stage->bundle[0].image[0] = tr.whiteImage;
                 } else {
                     stage->bundle[0].image[0] = tr.lightmaps[shader.lightmapIndex[0]];
@@ -881,6 +878,10 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
             else if ( !Q_stricmp( token, "equal" ) )
             {
                 depthFuncBits = GLS_DEPTHFUNC_EQUAL;
+            }
+            else if ( !Q_stricmp( token, "disable" ) )
+            {
+                depthFuncBits = GLS_DEPTHTEST_DISABLE;
             }
             else
             {
@@ -1305,6 +1306,11 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
             {
                 stage->alphaGen = AGEN_LIGHTING_SPECULAR;
             }
+            else if ( !Q_stricmp( token, "blend" ) )
+            {
+                stage->alphaGen = AGEN_BLEND; // FIXME BOE: Weird, SOF2 claims to be skipping this but still sets it (?)
+                ri.Printf(PRINT_WARNING, "alphaGen-type 'blend' is only used by SOF2 glass, skipping... (shader: %s)\n", shader.name);
+            }
             else if ( !Q_stricmp( token, "oneMinusVertex" ) )
             {
                 stage->alphaGen = AGEN_ONE_MINUS_VERTEX;
@@ -1395,6 +1401,66 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
 
             continue;
         }
+        else if ( !Q_stricmp( token, "surfaceSprites" ) )
+        {
+            char buffer[1024] = "";
+
+            while ( 1 )
+            {
+                token = COM_ParseExt( text, qfalse );
+                if ( token[0] == 0 )
+                    break;
+                Q_strcat( buffer, sizeof (buffer), token );
+                Q_strcat( buffer, sizeof (buffer), " " );
+            }
+
+            //ParseSurfaceSprites( buffer, stage ); // FIXME BOE: VERTIGON system.
+            ri.Printf(PRINT_WARNING, "WARNING: VERTIGON system not yet implemented (in shader '%s')\n", shader.name);
+
+            continue;
+        }
+        //
+        // ssFademax <fademax>
+        // ssFadescale <fadescale>
+        // ssVariance <varwidth> <varheight>
+        // ssHangdown
+        // ssSpurt <???>
+        // ssNoOffset <???>
+        // ssFaceflat <???>
+        // ssSpurtflat <???>
+        // ssAnyangle
+        // ssFaceup
+        // ssWind <wind>
+        // ssWindIdle <windidle>
+        // ssVertSkew <???>
+        // ssFXDuration <duration>
+        // ssFXGrow <growwidth> <growheight>
+        // ssFXAlphaRange <???>
+        // ssFXWeather <???>
+        // ssGore <???>
+        // ssGoreCentral <???>
+        //
+        else if ( !Q_stricmpn( token, "ss", 2 ) )
+        {
+            char buffer[1024] = "";
+            char param[128] = "";
+
+            Q_strncpyz(param, token, sizeof(param));
+
+            while ( 1 )
+            {
+                token = COM_ParseExt( text, qfalse );
+                if ( token[0] == 0 )
+                    break;
+                Q_strcat( buffer, sizeof (buffer), token );
+                Q_strcat( buffer, sizeof (buffer), " " );
+            }
+
+            //ParseSurfaceSpritesOptional( param, buffer, stage ); // FIXME BOE: VERTIGON system.
+            ri.Printf(PRINT_WARNING, "WARNING: VERTIGON system not yet implemented (in shader '%s')\n", shader.name);
+
+            continue;
+        }
         else
         {
             ri.Printf( PRINT_WARNING, "WARNING: unknown parameter '%s' in shader '%s'\n", token, shader.name );
@@ -1406,9 +1472,7 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
     // if cgen isn't explicitly specified, use either identity or identitylighting
     //
     if ( stage->rgbGen == CGEN_BAD ) {
-        if ( blendSrcBits == 0 ||
-            blendSrcBits == GLS_SRCBLEND_ONE ||
-            blendSrcBits == GLS_SRCBLEND_SRC_ALPHA ) {
+        if (blendSrcBits == GLS_SRCBLEND_ONE || blendSrcBits == GLS_SRCBLEND_SRC_ALPHA ) {
             stage->rgbGen = CGEN_IDENTITY_LIGHTING;
         } else {
             stage->rgbGen = CGEN_IDENTITY;
@@ -1427,7 +1491,7 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
     }
 
     // decide which agens we can skip
-    if ( stage->alphaGen == AGEN_IDENTITY ) {
+    if ( stage->alphaGen == AGEN_ENTITY ) {
         if ( stage->rgbGen == CGEN_IDENTITY
             || stage->rgbGen == CGEN_LIGHTING_DIFFUSE ) {
             stage->alphaGen = AGEN_SKIP;

commit d48e8a4c1441fc655298029300065a99f336a5a7

OpenGL2: Fixed regression in commit 2073a0c. There is no need to check for forced rgbGen when checking AGEN_CONST alphaGen.

code/renderergl2/tr_shade.c
@@ -586,10 +586,8 @@ static void ComputeShaderColors( shaderStage_t *pStage, vec4_t baseColor, vec4_t
         case AGEN_SKIP:
             break;
         case AGEN_CONST:
-            if ( forceRGBGen != CGEN_CONST ) {
-                baseColor[3] = pStage->constantColor[3] / 255.0f;
-                vertColor[3] = 0.0f;
-            }
+            baseColor[3] = pStage->constantColor[3] / 255.0f;
+            vertColor[3] = 0.0f;
             break;
         case AGEN_WAVEFORM:
             baseColor[3] = RB_CalcWaveAlphaSingle( &pStage->alphaWave );