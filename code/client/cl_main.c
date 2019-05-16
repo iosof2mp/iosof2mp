iosof2mp/iosof2mp

commit b7797c1e1e22bfb4cf45924138e064adced8fcd3

All: Added flrand as renderer library import function.

code/client/cl_main.c
@@ -3282,6 +3282,7 @@ void CL_InitRef( void ) {
     ri.IN_Restart = IN_Restart;
 
     ri.ftol = Q_ftol;
+    ri.flrand = flrand;
 
     ri.Sys_SetEnv = Sys_SetEnv;
     ri.Sys_GLimpSafeInit = Sys_GLimpSafeInit;

code/renderercommon/tr_public.h
@@ -178,6 +178,7 @@ typedef struct {
 
     // math
     long    (*ftol)(float f);
+    float   (*flrand)(float min, float max);
 
     // system stuff
     void    (*Sys_SetEnv)( const char *name, const char *value );

commit f1b99ecded64927c7c4c43758c94c27f1a9f7c14

All: Fixed regression in commit b7797c1. It is not necessary to explicitly import flrand in the renderers, the flrand implementation used in the vanilla SoF2MP renderer matches the one defined in q_math.c.

code/client/cl_main.c
@@ -3282,7 +3282,6 @@ void CL_InitRef( void ) {
     ri.IN_Restart = IN_Restart;
 
     ri.ftol = Q_ftol;
-    ri.flrand = flrand;
 
     ri.Sys_SetEnv = Sys_SetEnv;
     ri.Sys_GLimpSafeInit = Sys_GLimpSafeInit;

code/renderercommon/tr_public.h
@@ -178,7 +178,6 @@ typedef struct {
 
     // math
     long    (*ftol)(float f);
-    float   (*flrand)(float min, float max);
 
     // system stuff
     void    (*Sys_SetEnv)( const char *name, const char *value );