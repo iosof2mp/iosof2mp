iosof2mp/iosof2mp

commit 4a89e47106fafd45961110e2504052a9fe293242

All: Added math functions from SoF2Plus. This commit adds some missing math functions from SoF2Plus (sof2plus/sof2plus-engine@ed5765c). These functions are required for future implementations of e.g. Ghoul II, RMG and more. They match the behavior of the math functions in vanilla SoF2MP.
Cumulative changes:
- Added the flrand, irand and Q_irand random number routines.
- Added LerpVector math routine.
- Added the Q_powf math routine.
- Changed the Com_Clamp math routine, it now works with integers. The added
Com_Clampf math routine works with floats.

code/qcommon/q_math.c
@@ -160,6 +160,51 @@ float   Q_crandom( int *seed ) {
     return 2.0 * ( Q_random( seed ) - 0.5 );
 }
 
+// This is the VC libc version of rand() without multiple seeds per thread or 12 levels
+// of subroutine calls.
+// Both calls have been designed to minimise the inherent number of float <--> int
+// conversions and the additional math required to get the desired value.
+// eg the typical tint = (rand() * 255) / 32768
+// becomes tint = irand(0, 255)
+static uint32_t        holdrand = 0x89abcdef;
+
+void Rand_Init( int seed )
+{
+    holdrand = seed;
+}
+
+// Returns a float min <= x < max (exclusive; will get max - 0.00001; but never max)
+float flrand(float min, float max)
+{
+    float   result;
+
+    holdrand = (holdrand * 214013L) + 2531011L;
+    result = (float)(holdrand >> 17); // 0 - 32767 range
+    result = ((result * (max - min)) / (float)QRAND_MAX) + min;
+
+    return(result);
+}
+
+// Returns an integer min <= x <= max (ie inclusive)
+int irand( int min, int max )
+{
+    int     result;
+
+    assert((max - min) < QRAND_MAX);
+
+    max++;
+    holdrand = (holdrand * 214013L) + 2531011L;
+    result = holdrand >> 17;
+    result = ((result * (max - min)) >> 15) + min;
+
+    return result;
+}
+
+int Q_irand( int value1, int value2 )
+{
+    return irand(value1, value2);
+}
+
 //=======================================================
 
 signed char ClampChar( int i ) {
@@ -182,6 +227,28 @@ signed short ClampShort( int i ) {
     return i;
 }
 
+int Com_Clamp( int min, int max, int value )
+{
+    if ( value < min )
+    {
+        return min;
+    }
+    if ( value > max )
+    {
+        return max;
+    }
+    return value;
+}
+
+float Com_Clampf( float min, float max, float value ) {
+    if ( value < min ) {
+        return min;
+    }
+    if ( value > max ) {
+        return max;
+    }
+    return value;
+}
 
 // this isn't a real cheap function to call!
 int DirToByte( vec3_t dir ) {
@@ -528,7 +595,6 @@ float Q_fabs( float f ) {
 /*
 ===============
 LerpAngle
-
 ===============
 */
 float LerpAngle (float from, float to, float frac) {
@@ -545,6 +611,17 @@ float LerpAngle (float from, float to, float frac) {
     return a;
 }
 
+/*
+===============
+LerpVector
+===============
+*/
+void LerpVector ( vec3_t from, vec3_t to, float lerp, vec3_t out )
+{
+    out[0] = from[0] + (to[0]-from[0]) * lerp;
+    out[1] = from[1] + (to[1]-from[1]) * lerp;
+    out[2] = from[2] + (to[2]-from[2]) * lerp;
+}
 
 /*
 =================
@@ -865,6 +942,12 @@ void Vector4Scale( const vec4_t in, vec_t scale, vec4_t out ) {
     out[3] = in[3]*scale;
 }
 
+float Q_powf ( float x, int y ){
+    float r = x;
+    for ( y--; y>0; y-- )
+        r *= x;
+    return r;
+}
 
 int Q_log2( int val ) {
     int answer;
@@ -875,8 +958,6 @@ int Q_log2( int val ) {
     }
     return answer;
 }
-
-
 
 /*
 =================

code/client/cl_main.c
@@ -477,7 +477,7 @@ void CL_CaptureVoip(void)
     // try to get more audio data from the sound card...
 
     if (initialFrame) {
-        S_MasterGain(Com_Clamp(0.0f, 1.0f, cl_voipGainDuringCapture->value));
+        S_MasterGain(Com_Clampf(0.0f, 1.0f, cl_voipGainDuringCapture->value));
         S_StartCapture();
         CL_VoipNewGeneration();
         CL_VoipParseTargets();
@@ -2968,8 +2968,8 @@ void CL_Frame ( int msec ) {
     if ( CL_VideoRecording( ) && cl_aviFrameRate->integer && msec) {
         // save the current screen
         if ( clc.state == CA_ACTIVE || cl_forceavidemo->integer) {
-            float fps = MIN(cl_aviFrameRate->value * com_timescale->value, 1000.0f);
-            float frameDuration = MAX(1000.0f / fps, 1.0f) + clc.aviVideoFrameRemainder;
+            float fps = min(cl_aviFrameRate->value * com_timescale->value, 1000.0f);
+            float frameDuration = max(1000.0f / fps, 1.0f) + clc.aviVideoFrameRemainder;
 
             CL_TakeVideoFrame( );
 

code/client/snd_dma.c
@@ -1245,8 +1245,8 @@ void S_GetSoundtime(void)
 
     if( CL_VideoRecording( ) )
     {
-        float fps = MIN(cl_aviFrameRate->value, 1000.0f);
-        float frameDuration = MAX(dma.speed / fps, 1.0f) + clc.aviSoundFrameRemainder;
+        float fps = min(cl_aviFrameRate->value, 1000.0f);
+        float frameDuration = max(dma.speed / fps, 1.0f) + clc.aviSoundFrameRemainder;
 
         int msec = (int)frameDuration;
         s_soundtime += msec;

code/client/snd_mix.c
@@ -176,7 +176,7 @@ void S_TransferPaintBuffer(int endtime)
         p = (int *) paintbuffer;
         count = (endtime - s_paintedtime) * dma.channels;
         out_idx = (s_paintedtime * dma.channels) % dma.samples;
-        step = 3 - MIN(dma.channels, 2);
+        step = 3 - min(dma.channels, 2);
 
         if ((dma.isfloat) && (dma.samplebits == 32))
         {

code/game/g_bot.c
@@ -775,7 +775,7 @@ void Svcmd_AddBot_f( void ) {
         skill = 4;
     }
     else {
-        skill = Com_Clamp( 1, 5, atof( string ) );
+        skill = Com_Clampf( 1, 5, atof( string ) );
     }
 
     // team

code/qcommon/common.c
@@ -2716,6 +2716,9 @@ void Com_Init( char *commandLine ) {
     Com_InitZoneMemory();
     Cmd_Init ();
 
+    // Seed the random number generator
+    Rand_Init(Sys_Milliseconds());
+
     // get the developer cvar set as early as possible
     com_developer = Cvar_Get("developer", "0", CVAR_TEMP);
 

code/qcommon/q_shared.c
@@ -23,17 +23,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // q_shared.c -- stateless support routines that are included in each code dll
 #include "q_shared.h"
 
-float Com_Clamp( float min, float max, float value ) {
-    if ( value < min ) {
-        return min;
-    }
-    if ( value > max ) {
-        return max;
-    }
-    return value;
-}
-
-
 /*
 ============
 COM_SkipPath

code/qcommon/q_shared.h
@@ -536,8 +536,10 @@ float Q_rsqrt( float f );       // reciprocal square root
 
 #define SQRTFAST( x ) ( (x) * Q_rsqrt( x ) )
 
-signed char ClampChar( int i );
-signed short ClampShort( int i );
+signed char     ClampChar   ( int i );
+signed short    ClampShort  ( int i );
+int             Com_Clamp   ( int min, int max, int value );
+float           Com_Clampf  ( float min, float max, float value );
 
 // this isn't a real cheap function to call!
 int DirToByte( vec3_t dir );
@@ -677,13 +679,21 @@ vec_t VectorNormalize (vec3_t v);       // returns vector length
 vec_t VectorNormalize2( const vec3_t v, vec3_t out );
 void Vector4Scale( const vec4_t in, vec_t scale, vec4_t out );
 void VectorRotate( vec3_t in, vec3_t matrix[3], vec3_t out );
-int Q_log2(int val);
 
-float Q_acos(float c);
+float   Q_powf  ( float x, int y );
+int     Q_log2  ( int val );
+float   Q_acos  ( float c );
 
-int     Q_rand( int *seed );
-float   Q_random( int *seed );
-float   Q_crandom( int *seed );
+#define QRAND_MAX   32768
+
+int     Q_rand      ( int *seed );
+float   Q_random    ( int *seed );
+float   Q_crandom   ( int *seed );
+
+void    Rand_Init   ( int seed );
+float   flrand      ( float min, float max );
+int     irand       ( int min, int max );
+int     Q_irand     ( int value1, int value2 );
 
 #define random()    ((rand () & 0x7fff) / ((float)0x7fff))
 #define crandom()   (2.0 * (random() - 0.5))
@@ -706,6 +716,7 @@ qboolean BoundsIntersectPoint(const vec3_t mins, const vec3_t maxs,
 
 float   AngleMod(float a);
 float   LerpAngle (float from, float to, float frac);
+void    LerpVector ( vec3_t from, vec3_t to, float lerp, vec3_t out );
 float   AngleSubtract( float a1, float a2 );
 void    AnglesSubtract( vec3_t v1, vec3_t v2, vec3_t v3 );
 
@@ -726,17 +737,15 @@ void MatrixMultiply(float in1[3][3], float in2[3][3], float out[3][3]);
 void AngleVectors( const vec3_t angles, vec3_t forward, vec3_t right, vec3_t up);
 void PerpendicularVector( vec3_t dst, const vec3_t src );
 
-#ifndef MAX
-#define MAX(x,y) ((x)>(y)?(x):(y))
+#ifndef max
+#define max(x,y) ((x)>(y)?(x):(y))
 #endif
 
-#ifndef MIN
-#define MIN(x,y) ((x)<(y)?(x):(y))
+#ifndef min
+#define min(x,y) ((x)<(y)?(x):(y))
 #endif
 
 //=============================================
-
-float Com_Clamp( float min, float max, float value );
 
 char    *COM_SkipPath( char *pathname );
 const char  *COM_GetExtension( const char *name );

commit f153de0daa5e3857c8d279ab140b76171c6df16c

All: Fixed regression in commit 4a89e47. The QVM modules failed to compile due to the following regressions: - MIN and MAX definitions that were changed to min and max for vanilla SoF2MP SDK compatibility, they were however not changed in the current VM modules. - The usage of the fixed integer type uint32_t in QVMs, an unsigned int is now used when compiled as QVM module. - The usage of assert in the irand function, it is now only used when compiling the VM modules as native library.

code/game/bg_lib.c
@@ -163,9 +163,9 @@ loop:   SWAPINIT(a, es);
     }
 
     pn = (char *)a + n * es;
-    r = MIN(pa - (char *)a, pb - pa);
+    r = min(pa - (char *)a, pb - pa);
     vecswap(a, pb - r, r);
-    r = MIN(pd - pc, pn - pd - es);
+    r = min(pd - pc, pn - pd - es);
     vecswap(pb, pn - r, r);
     if ((r = pb - pa) > es)
         qsort(a, r / es, es, cmp);
@@ -1840,12 +1840,12 @@ static int fmtint (char *buffer, size_t *currlen, size_t maxlen,
   convert[place] = 0;
 
   zpadlen = max - place;
-  spadlen = min - MAX (max, place) - (signvalue ? 1 : 0);
+  spadlen = min - max (max, place) - (signvalue ? 1 : 0);
   if (zpadlen < 0) zpadlen = 0;
   if (spadlen < 0) spadlen = 0;
   if (flags & DP_F_ZERO)
   {
-    zpadlen = MAX(zpadlen, spadlen);
+    zpadlen = max(zpadlen, spadlen);
     spadlen = 0;
   }
   if (flags & DP_F_MINUS)

code/qcommon/q_math.c
@@ -166,7 +166,12 @@ float   Q_crandom( int *seed ) {
 // conversions and the additional math required to get the desired value.
 // eg the typical tint = (rand() * 255) / 32768
 // becomes tint = irand(0, 255)
-static uint32_t        holdrand = 0x89abcdef;
+
+#ifndef Q3_VM
+static uint32_t     holdrand = 0x89abcdef;
+#else
+static unsigned int holdrand = 0x89abcdef;
+#endif
 
 void Rand_Init( int seed )
 {
@@ -190,7 +195,9 @@ int irand( int min, int max )
 {
     int     result;
 
+#ifndef Q3_VM
     assert((max - min) < QRAND_MAX);
+#endif
 
     max++;
     holdrand = (holdrand * 214013L) + 2531011L;